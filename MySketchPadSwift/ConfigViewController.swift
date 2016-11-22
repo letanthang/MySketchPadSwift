fea487fbd16b0f1ef514d21615a1fe929b361ac4
Le Tan Thang <letanthang@gmail.com>
2016-11-22 14:48:42 +0700

    export image & config GUI

---
+++ MySketchPadSwift/ConfigViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ConfigViewController.swift
+//  MySketchPadSwift
+//
+//  Created by Le Tan Thang on 11/22/16.
+//  Copyright © 2016 Le Tan Thang. All rights reserved.
+//
+
+import UIKit
+
+class ConfigViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- MySketchPadSwift/ViewController.swift
+++ MySketchPadSwift/ViewController.swift
@@ -25,6 +25,41 @@ class ViewController: UIViewController {
     override var prefersStatusBarHidden: Bool {
         return true
     }
+    
+    @IBAction func undoTapped(_ sender: Any) {
+    }
+    
+    @IBAction func redoTapped(_ sender: Any) {
+    }
+    
+    @IBAction func saveTapped(_ sender: Any) {
+        // image to share
+        guard let image = self.captureView() else { return }
+        
+        // set up activity view controller
+        let imageToShare = [ image ]
+        let activityViewController = UIActivityViewController(activityItems: imageToShare, applicationActivities: nil)
+        activityViewController.popoverPresentationController?.sourceView = self.view // so that iPads won't crash
+        
+        // exclude some activity types from the list (optional)
+        activityViewController.excludedActivityTypes = [ UIActivityType.airDrop, UIActivityType.postToFacebook ]
+        
+        // present the view controller
+        self.present(activityViewController, animated: true, completion: nil)
+    }
+    func captureView() -> UIImage? {
+    
+        let rect = self.containerView.bounds
+        var img: UIImage? = nil
+        UIGraphicsBeginImageContext(rect.size)
+        if let context = UIGraphicsGetCurrentContext() {
+            self.containerView.layer.render(in: context)
+            img = UIGraphicsGetImageFromCurrentImageContext()
+        }
+        UIGraphicsEndImageContext()
+        return img
+    }
+    
 
 }
 
